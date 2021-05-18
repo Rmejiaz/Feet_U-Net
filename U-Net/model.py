c555bc849a7d4f46d126e1bc593ea6295bdf69f2
Rafael Mejia <rmejiaz@unal.edu.co>
2021-05-18 14:10:05 -0500

    Added model.py and train.py

--- U-Net/model.py
+++ U-Net/model.py
@@ -1,69 +1,76 @@
+"""
+U-Net implementation to segment feet and background
+"""
+import cv2
+import os
+import numpy as np
+import matplotlib.pyplot as plt
 import tensorflow as tf
 
-def upsample(filters,size,strides=2,padding="same",batchnorm=False,dropout=False):
+from os import path
+from tensorflow import keras
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras.optimizers import Adam, RMSprop
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.metrics import BinaryAccuracy
+from tensorflow.keras.layers import ( Conv2D,  Conv2DTranspose, 
+                                    Input,  MaxPool2D, 
+                                    UpSampling2D,  Concatenate
+                                    )
 
-    layer = tf.keras.Sequential()
-    layer.add(
-        tf.keras.layers.Conv2DTranspose(filters,size,strides,padding,use_bias = False))
 
-    if batchnorm:
-        layer.add(tf.keras.layers.BatchNormalization())
+def UNET2D(input_size = (128, 128, 1)):
 
-    if dropout:
-        layer.add(tf.keras.layers.Dropout(0.3))
+    inputs = Input(shape= input_size)
 
-    layer.add(tf.keras.layers.ReLU())
+    ## Encoder
+    conv1 = Conv2D(64, 3, activation = "relu", padding = "same")(inputs)
+    conv1 = Conv2D(64, 3, activation = "relu", padding = "same")(conv1)
+    pool1 = MaxPool2D(pool_size = (2, 2))(conv1)
 
-    return layer
+    conv2 = Conv2D(128, 3, activation = "relu", padding = "same")(pool1)
+    conv2 = Conv2D(128, 3, activation = "relu", padding = "same")(conv2)
+    pool2 = MaxPool2D(pool_size = (2, 2))(conv2)
 
-def get_encoder(input_shape=[None,None,3],name="encoder"): 
-    Input = tf.keras.layers.Input(shape=input_shape)
-    base_model = tf.keras.applications.MobileNetV2(input_tensor=Input, include_top=False)
-    layer_names = [
-    'block_1_expand_relu',   # 64x64
-    'block_3_expand_relu',   # 32x32
-    'block_6_expand_relu',   # 16x16
-    'block_13_expand_relu',  # 8x8
-    'block_16_project',      # 4x4
-    ]
-    layers = [base_model.get_layer(name).output for name in layer_names]
+    conv3 = Conv2D(256, 3, activation = "relu", padding = "same")(pool2)
+    conv3 = Conv2D(256, 3, activation = "relu", padding = "same")(conv3)
+    pool3 = MaxPool2D(pool_size = (2, 2))(conv3)
 
-    # Create the feature extraction model
-    encoder  = tf.keras.Model(inputs=Input, outputs=layers,name=name)
-    encoder.trainable = False
+    conv4 = Conv2D(512, 3, activation = "relu", padding = "same")(pool3)
+    conv4 = Conv2D(512, 3, activation = "relu", padding = "same")(conv4)
+    pool4 = MaxPool2D(pool_size = (2, 2))(conv4)
 
-    return encoder
+    conv5 = Conv2D(1024, 3, activation = "relu", padding = "same")(pool4)
+    conv5 = Conv2D(1024, 3, activation = "relu", padding = "same")(conv5)
 
-def get_decoder(skips):
-    up_stack = [
-        upsample(512, 3,dropout=False),  # 4x4 -> 8x8
-        upsample(256, 3,dropout=False),  # 8x8 -> 16x16
-        upsample(128, 3,dropout=False),  # 16x16 -> 32x32
-        upsample(64, 3,dropout=False),   # 32x32 -> 64x64
-    ]
-    x = skips[-1]
-    skips = reversed(skips[:-1])
+    # Decoder
 
-    for up,skip in zip(up_stack,skips):
-        x = up(x)
-        x = tf.keras.layers.Concatenate()([x,skip])
-    return x
+    up6 = UpSampling2D(size = (2, 2))(conv5)
+    up6 = Conv2DTranspose(512, 2, padding = "same")(up6)
+    conc6 = Concatenate()([conv4, up6])
+    conv6 = Conv2D(512, 3, activation = "relu", padding = "same")(up6)
+    conv6 = Conv2D(512, 3, activation = "relu", padding = "same")(conc6)
 
-def get_model(output_channels=1,size=128,name="U-Net"):
-    x = inputs = tf.keras.layers.Input(shape=[size,size,3])
+    up7 = UpSampling2D(size = (2, 2))(conv6)
+    up7 = Conv2DTranspose(256, 2, padding = "same")(up7)
+    conc7 = Concatenate()([conv3, up7])
+    conv7 = Conv2D(256, 3, activation = "relu", padding = "same")(up7)
+    conv7 = Conv2D(256, 3, activation = "relu", padding = "same")(conc7)
 
-    skips = get_encoder(input_shape=list(x.shape[1:]))(x)
+    up8 = UpSampling2D(size = (2, 2))(conv7)
+    up8 = Conv2DTranspose(128, 2, padding = "same")(up8)
+    conc8 = Concatenate()([conv2, up8])
+    conv8 = Conv2D(128, 3, activation = "relu", padding = "same")(up8)
+    conv8 = Conv2D(128, 3, activation = "relu", padding = "same")(conc8)
 
-    x = get_decoder(skips)
+    up9 = UpSampling2D(size = (2, 2))(conv8)
+    up9 = Conv2DTranspose(64, 2, padding = "same")(up9)
+    conc9 = Concatenate()([conv1, up9])
+    conv9 = Conv2D(64, 3, activation = "relu", padding = "same")(up9)
+    conv9 = Conv2D(64, 3, activation = "relu", padding = "same")(conc9)
 
-    last = tf.keras.layers.Conv2DTranspose(
-        output_channels, 3, strides=2,
-        padding='same',activation=tf.keras.activations.sigmoid)  #64x64 -> 128x128
+    out = Conv2D(1, 1, activation = "sigmoid")(conv9)
+    model = keras.Model(inputs=inputs, outputs=out)
 
-    x = last(x)
-    return tf.keras.Model(inputs=inputs, outputs=x,name=name)
-
-if __name__ == '__main__':
-    model = get_model(output_channels=2)
-    model.summary()
-    tf.keras.utils.plot_model(model,to_file='data/model.png',show_shapes=False,show_layer_names=False)
+                  
+    return model
--- U-Net/train.py
+++ U-Net/train.py
@@ -1,3 +1,7 @@
+"""
+Training using the model defined in model2.py
+"""
+
 from absl import app, flags, logging
 from absl.flags import FLAGS
 import tensorflow as tf
@@ -7,14 +11,17 @@ import utils
 import os 
 from tensorflow.keras import backend as K
 import numpy as np
+from model2 import UNET2D
+from tensorflow.keras.utils import plot_model
 
-flags.DEFINE_string('train_Dataset','./tfrecords/train-data.tfrecord','path to train Dataset')
+
 flags.DEFINE_float('val_split',0.2,'size of the validation split')
 flags.DEFINE_string('weights','./weights/','path to save the model weights')
 flags.DEFINE_integer('buffer_size', 100, 'buffer')
 flags.DEFINE_integer('batch_size', 5, 'batch size')
 flags.DEFINE_integer('epochs', 10, 'Epochs')
 flags.DEFINE_integer('save_freq', 5, 'frequency of epochs to save')
+
 
 def dice_coef(y_true, y_pred):
   intersection = K.sum(y_true * y_pred, axis=[1,2,3])
@@ -27,53 +34,49 @@ def main(_argv):
 
     # Initialize variables
     checkpoint_path = FLAGS.weights+'cp-{epoch:04d}.ckpt'
-    image_size = 224
-    classes = 1
-    train_Dataset_path = FLAGS.train_Dataset
 
+    image_size = 128
+    n_channels = 1
+    ## Load the data
 
-    # Load train dataset
-    
-    # train_Dataset =  utils.load_tfrecord_dataset(train_Dataset_path,image_size)
-    # train_Dataset = train_Dataset.shuffle(buffer_size=FLAGS.buffer_size)
     X_path = "./Dataset_Unificado/Train/Processed_Images"
     Y_path = "./Dataset_Unificado/Train/BinaryMasks"
     X = utils.load_data(X_path,size=image_size)
     Y = utils.load_data(Y_path,size=image_size)
+
+    X = X[:,:,:,0]
     Y = Y[:,:,:,0]
+    X = np.expand_dims(X,axis=-1)
     Y = np.expand_dims(Y,axis=-1)
 
-    # Split train and validation according to val_split
+    ## Load the model
 
-    # train_Dataset, val_Dataset = utils.split_dataset(train_Dataset,FLAGS.val_split)
+    model = UNET2D(input_size = (image_size,image_size,n_channels))
 
-    # Batch and prefetch
-
-    # train_Dataset = train_Dataset.batch(FLAGS.batch_size,drop_remainder=True)
-    # train_Dataset = train_Dataset.prefetch(buffer_size = tf.data.experimental.AUTOTUNE)
-    # val_Dataset = val_Dataset.batch(FLAGS.batch_size,drop_remainder=True)
-    # val_Dataset = val_Dataset.prefetch(buffer_size = tf.data.experimental.AUTOTUNE)
-
-    # Load model
-
-    # n_batches_per_epoch = len(list(train_Dataset.as_numpy_iterator()))
+    ## Compile model
+    
     n_batches_per_epoch = X.shape[0]
     save_freq = int(n_batches_per_epoch * FLAGS.save_freq)
 
+    model.compile(
+                optimizer = tf.keras.optimizers.RMSprop(),
+                metrics = [tf.keras.metrics.MeanIoU(num_classes = 2),dice_coef,tf.keras.metrics.BinaryAccuracy(name='BinaryAccuracy')],
+                loss = tf.keras.losses.BinaryCrossentropy()
+                )
+
+
+    ## Create callbacks
+    # save weights
     cp_callback = tf.keras.callbacks.ModelCheckpoint(
                                                     filepath = checkpoint_path,
                                                     verbose=1,
                                                     save_weights_only = True,
                                                     save_freq = save_freq
                                                     )
-
-    model = get_model(output_channels = classes, size = image_size)
+    
     model.save_weights(checkpoint_path.format(epoch=0))
-    model.compile(
-                optimizer = tf.keras.optimizers.Adam(),
-                metrics = [dice_coef],
-                loss = tf.keras.losses.BinaryCrossentropy()
-                )
+    
+    # early stop callback
 
     # Train the model
     model_history = model.fit(
@@ -111,3 +114,6 @@ def main(_argv):
 
 if __name__ == '__main__':
     app.run(main)
+
+    
+
