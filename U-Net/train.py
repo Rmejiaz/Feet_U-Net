60f0def3f40422bb8eedebd7ce44b18fee9ce553
Rafael Mejía <rmejiaz@unal.edu.co>
2021-05-19 16:16:59 -0500

    Modified train.py

--- U-Net/train.py
+++ U-Net/train.py
@@ -9,7 +9,10 @@ from tensorflow.keras import backend as K
 import numpy as np
 from utils import DiceSimilarity, jaccard
 
-flags.DEFINE_string('train_Dataset','./tfrecords/train-data.tfrecord','path to train Dataset')
+flags.DEFINE_string('imgs_path','./Dataset_Unificado/Train/Processed_Images','path to the training images')
+flags.DEFINE_string('masks_path','./Dataset_Unificado/Train/BinaryMasks','path to the training masks')
+flags.DEFINE_string('val_masks','./Dataset_Unificado/Test/BinaryMasks','path to the validation masks')
+flags.DEFINE_string('val_imgs','./Dataset_Unificado/Test/Processed_Images','path to the validation images')
 flags.DEFINE_float('val_split',0.2,'size of the validation split')
 flags.DEFINE_string('weights','./weights/','path to save the model weights')
 flags.DEFINE_integer('buffer_size', 100, 'buffer')
@@ -24,26 +27,28 @@ def main(_argv):
     checkpoint_path = FLAGS.weights+'cp-{epoch:04d}.ckpt'
     image_size = 224
     classes = 1
-    train_Dataset_path = FLAGS.train_Dataset
-
-
+    X_path = FLAGS.imgs_path
+    Y_path = FLAGS.masks_path
+    X_val_path = FLAGS.val_imgs
+    Y_val_path = FLAGS.val_imgs
+    val_split = FLAGS.val_split
     # Load train dataset
     
-    X_path = "./Dataset_Unificado/Train/Processed_Images"
-    Y_path = "./Dataset_Unificado/Train/BinaryMasks"
     X = utils.load_data(X_path,size=image_size)
     Y = utils.load_data(Y_path,size=image_size)
     Y = Y[:,:,:,0]
     Y = np.expand_dims(Y,axis=-1)
 
     # Load test dataset
-
-    X_val_path = "./Dataset_Unificado/Test/Processed_Images"
-    Y_val_path = "./Dataset_Unificado/Test/BinaryMasks"
-    X_val = utils.load_data(X_val_path,size=image_size)
-    Y_val = utils.load_data(Y_val_path, size=image_size)
-    Y_val = Y_val[:,:,:,0]
-    Y_val = np.expand_dims(Y_val,axis=-1)
+    try:
+        X_val_path = "./Dataset_Unificado/Test/Processed_Images"
+        Y_val_path = "./Dataset_Unificado/Test/BinaryMasks"
+        X_val = utils.load_data(X_val_path,size=image_size)
+        Y_val = utils.load_data(Y_val_path, size=image_size)
+        Y_val = Y_val[:,:,:,0]
+        Y_val = np.expand_dims(Y_val,axis=-1)
+    except:
+        pass 
 
     # Load model
 
@@ -67,15 +72,25 @@ def main(_argv):
                 )
 
     # Train the model
-    model_history = model.fit(
-                            x = X,
-                            y = Y,
-                            validation_data = (X_val,Y_val),
-                            epochs =FLAGS.epochs,
-                            batch_size = FLAGS.batch_size,
-                            callbacks = [cp_callback]
-                             )
-
+    
+    if val_split == 0:
+        model_history = model.fit(
+                                x = X,
+                                y = Y,
+                                validation_data = (X_val,Y_val),
+                                epochs =FLAGS.epochs,
+                                batch_size = FLAGS.batch_size,
+                                callbacks = [cp_callback]
+                                )
+    else:
+        model_history = model.fit(
+                                x = X,
+                                y = Y,
+                                validation_split = val_split,
+                                epochs =FLAGS.epochs,
+                                batch_size = FLAGS.batch_size,
+                                callbacks = [cp_callback]
+                                )
     # Create the results directory
     if  not 'results' in os.listdir():
         os.mkdir('results')
